8e6e098c0fcd5d1059816ba007ddd00669e59fe7
Joe <joe7.world@gmail.com>
2017-03-15 19:09:40 +0300

    les1.2

---
+++ src/dz/dz1/e/somov/MyChess.java
@@ -0,0 +1,15 @@
+package dz.dz1.e.somov;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.Scanner;
+
+/**
+ * Created by spo_admin4 on 15.03.2017.
+ */
+public class MyChess {
+    public static void main(String[] args) {
+
+        new PlayChess();
+    }
+}
---
+++ src/dz/dz1/e/somov/PlayChess.java
@@ -0,0 +1,17 @@
+package dz.dz1.e.somov;
+
+import java.util.Scanner;
+
+/**
+ * Created by spo_admin4 on 15.03.2017.
+ */
+public class PlayChess {
+    int[][] field = new int[8][8];
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void playerTurn() {
+        System.out.println("Выбирете фигуру: 1-Castle, 2-Knight, 3-Bishop");
+        //int chfig = sc.nextlnt();
+    }
+}
