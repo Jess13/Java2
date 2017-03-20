834014ff1313b0112cc1572b6a111171b4806086
Joe <joe7.world@gmail.com>
2017-03-20 14:31:54 +0300

    les2.1

--- src/dz/dz2/esomov/MyException.java
+++ src/dz/dz2/esomov/MyException.java
@@ -7,38 +7,37 @@ import java.util.Scanner;
  */
 public class MyException {
 
-    //Преобразование строки в массив типа String
-    private static String[][] str2mas(String str) {
-        String[] mas1 = str.split("\n");
-        String[][] mas2 = new String[mas1.length][];
+    //Преобразование строки в массив типа String, приведение к типу Int и арифметические вычисления
+    private static int str2mas(String str) {
+        int sum = 0; //подсчет суммы всех элементов массива
+        String[] mas1 = str.split("\n"); //массив из строки, разделенной "\n"
+        String[][] mas2 = new String[mas1.length][]; //массив из массива строк, разделенные " "
+        int[][] mas3; //массив из чисел, нельзя инициировать new int[mas1.length][], выдаст ошибку пустого индекса
+
         for (int i=0; i<mas1.length; i++){
             mas2[i] = mas1[i].split(" ");
-        }
-        if (mas1.length != 4 || mas2.length != 4) throw new ArrayIndexOutOfBoundsException("Размер матрицы не равен 4Х4");
-        return mas2;
-    }
+            mas3 = new int[mas1.length][mas2[i].length]; //инициируем mas3
+            if (mas1.length != 4 || mas2[i].length != 4) throw new ArrayIndexOutOfBoundsException("Размер матрицы не равен 4Х4");
 
-    //Преобразование массива типа String в тип Int
-    private static int str2int (String[][] mas) {
-        int[][] mas3 = new int[mas.length][mas.length];
-        //TODO Переработать перебор массива в цикле!!!
-        int sum = 0;
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<4; j++) {
-                mas3[i][j] = Integer.parseInt(mas[i][j]);
-                System.out.print(mas3[i][j]+"   ");
-                sum += mas3[i][j];
+            for (int j=0; j<mas2[i].length; j++) {
+                try {
+                    mas3[i][j] = Integer.parseInt(mas2[i][j]);
+                    sum += mas3[i][j];
+                }catch (NumberFormatException e) {
+                    //e.printStackTrace();
+                    System.out.println("Введено не число! "+e);
+                    return 0;
+                }
+//                System.out.print(mas2[i][j]+" ");
             }
         }
-        System.out.println(sum/2);
         return sum/2;
     }
 
     public static void main(String[] args) {
-
 //        for (String element: str1){ System.out.println(element); }
-        String str = "1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0 1 1 4 5 3 5 v d t e";
-        String[][] mas = str2mas(str);
-        str2int(mas);
+        String str = "1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0";
+        int res = str2mas(str);
+        System.out.println("resultat="+res);
     }
 }
