d3912c5771feb7fe2307619f81916af12157e651
Joe <joe7.world@gmail.com>
2017-03-27 16:15:48 +0300

    les4.2

--- src/ru/geekbrains/java2/dz/dz4/esomov/MyWindow.java
+++ src/ru/geekbrains/java2/dz/dz4/esomov/MyWindow.java
@@ -6,15 +6,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by Jess on 25.03.2017.
  */
 class MyWindow extends JFrame{
 
-    private static String FileBD = "src\\ru\\geekbrains\\java2\\dz\\dz4\\esomov\\History.txt";
-    private static JTextArea ta;
-    private static JTextField t;
+    private static String FileBD = "src\\ru\\geekbrains\\java2\\dz\\dz4\\esomov\\History.txt"; //Файл переписки
+    private static JTextArea ta; //Поле для всех сообщений
+    private static JTextField t; //Поле для ввода сообщений
 
     MyWindow(){
         JFrame f = new JFrame("Chat");
@@ -32,19 +35,19 @@ class MyWindow extends JFrame{
         JButton snd = new JButton("Send");
         JButton upd = new JButton("Update");
 
-        t.addActionListener(new ActionListener() {
+        t.addActionListener(new ActionListener() { //Обработка нажатия клавиши Enter
             @Override
             public void actionPerformed(ActionEvent e) {
                 sendText();
             }
         });
-        snd.addActionListener(new ActionListener() {
+        snd.addActionListener(new ActionListener() { //Обработка нажатия клавиши Send
             @Override
             public void actionPerformed(ActionEvent e) {
                 sendText();
             }
         });
-        upd.addActionListener(new ActionListener() {
+        upd.addActionListener(new ActionListener() { //Обработка нажатия клавиши Update
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
@@ -66,40 +69,31 @@ class MyWindow extends JFrame{
         f.setVisible(true);
     }
 
+    //Добаление текста в поле TextArea и запись в файл
     private void sendText()  {
-        String sText = t.getText();
-        if (!sText.equals("")) {
-            sText += "\n";
-            ta.append(sText);
-            writeChat(sText);
-            t.setText(null);
+        String sText = t.getText(); //получений значения из TextField
+        if (!sText.equals("")) { //проверка на пустой ввод данных
+            sText += "\n"; //Запись перевода строки
+            ta.append(sText); //Добаление сообщения в TextArea
+            writeChat(sText); //Запись сообщения в файл
+            t.setText(null); //Очистить поле TextField
         }
     }
 
+    //Чтение из файла в поле TextArea - отображение переписки
     private void readChat() throws IOException {
-        FileReader fileReader = new FileReader(FileBD);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String inputFile = "";
-        String textFieldReadable = bufferedReader.readLine();
-        try {
-            while (textFieldReadable != null) {
-                inputFile += textFieldReadable;
-                textFieldReadable = bufferedReader.readLine();
-            }
-            ta.setText(inputFile);
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не существует!");
-        }
-        catch (IOException e) {
-            System.out.println("Ошибка Ввода/Вывода!");
-        }
+        java.util.List<String> lines = Files.readAllLines(Paths.get(FileBD), StandardCharsets.UTF_8);
+        String input = ""; //запись всех строк в input
+        for(String line: lines) input += line+"\n"; //Чтение каждой строки с добавлением перевода
+        ta.setText(input); //добавление текста в TextArea - чат
     }
 
+    //Запись введенного сообщения из TextField в файл
     private void writeChat(String data) {
-        File file = new File(FileBD);
+        File file = new File(FileBD); //создание или перезапись файла для переписки
         FileWriter fr = null;
         try {
-            fr = new FileWriter(file, true);
+            fr = new FileWriter(file, true); //append true - текст добавляется, а не перезаписывается
             fr.write(data);
             fr.flush();
         } catch (IOException e) {
