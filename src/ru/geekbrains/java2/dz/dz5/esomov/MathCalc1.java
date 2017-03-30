109571bde9b8fb851b2efa29441ee224bb0ab993
Joe <joe7.world@gmail.com>
2017-03-30 17:30:46 +0300

    les5.1

--- src/ru/geekbrains/java2/dz/dz5/esomov/MathCalc1.java
+++ src/ru/geekbrains/java2/dz/dz5/esomov/MathCalc1.java
@@ -11,7 +11,6 @@ public class MathCalc1 implements Runnable {
         long a = System.currentTimeMillis();
         for (int i=1; i<arr.length; i++) arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         System.currentTimeMillis();
-        System.out.println("MathCalc1 Completed!");
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("MathCalc1 Completed, with: "+(System.currentTimeMillis() - a)+" MilliSec");
     }
 }
--- src/ru/geekbrains/java2/dz/dz5/esomov/MathCalc2.java
+++ src/ru/geekbrains/java2/dz/dz5/esomov/MathCalc2.java
@@ -7,16 +7,28 @@ public class MathCalc2 implements Runnable {
     @Override
     public void run() {
         float[] arr = new float[Main.MAS_SIZE];
-        float[] a1 = new float[Main.H];
-        float[] a2 = new float[Main.H];
+        float[] arr1 = new float[Main.H];
+        float[] arr2 = new float[Main.H];
         for (int i=1; i<arr.length; i++) arr[i]=1.00f;
-        long a = System.currentTimeMillis();
-        System.arraycopy(arr, 0, a1, 0, Main.H);
-        System.arraycopy(arr, Main.H, a2, 0, Main.H);
-        for (int i=1; i<a1.length; i++) a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        for (int i=1; i<a2.length; i++) a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        System.currentTimeMillis();
-        System.out.println("MathCalc2 Completed!");
-        System.out.println(System.currentTimeMillis() - a);
+        long a1,a2;
+
+        a1 = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, Main.H);
+        System.arraycopy(arr, Main.H, arr2, 0, Main.H);
+
+        Thread thread3 = new Thread(() -> {
+            for (int i=1; i<arr1.length; i++) arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        });
+        thread3.start();
+
+        Thread thread4 = new Thread(() -> {
+            for (int i=1; i<arr2.length; i++) arr2[i] = (float)(arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        });
+        thread4.start();
+
+        System.arraycopy(arr1, 0, arr, 0, Main.H);
+        System.arraycopy(arr2, 0, arr, Main.H, Main.H);
+        a2 = System.currentTimeMillis();
+        System.out.println("MathCalc2 Completed, with: "+(a2 - a1)+" MilliSec");
     }
 }
