63439b71d135d4e82dcb7f6f40d03ca8aebdf941
unknown <joe7.world@gmail.com>
2017-04-19 01:25:11 +0300

    les8

---
+++ src/main/java/ru/geekbrains/java2/lesson8/server/SQLHandler.java
@@ -0,0 +1,74 @@
+package ru.geekbrains.java2.lesson8.server;
+
+import java.sql.*;
+
+public class SQLHandler {
+    private static final String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String user = "postgres";
+    private static final String password = "postgres"; // наверно нехорошо делать пароль публичным, но подругому не получилось
+    private static final String SQL_SELECT = "SELECT Nickname FROM main WHERE Login = ? AND Password = ?;";
+    private static final String SQL_INSERT = "INSERT INTO main (password, nickname, login) values (?, ?, ?);";
+    private static final String SQL_UPDATE = "UPDATE main SET password = ? WHERE login = ?;";
+
+    private static Connection conn;
+    private static PreparedStatement stmt;
+
+    public static void connect() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            conn = DriverManager.getConnection(dbUrl, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            conn.close();
+        } catch (Exception c) {
+            System.out.println("Connection Error");
+        }
+    }
+
+    public static String getNickByLoginPassword(String login, String password) {
+        String w = null;
+        try {
+            stmt = conn.prepareStatement(SQL_SELECT);
+            stmt.setString(1, login);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                w = rs.getString("Nickname");
+        } catch (SQLException e) {
+            System.out.println("SQL Query Error");
+        }
+        return w;
+    }
+
+    public static String addNewUser(String password, String nickname, String login) {
+        String word = null;
+        try {
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, password);
+            stmt.setString(2, nickname);
+            stmt.setString(3, login);
+            stmt.execute();
+        } catch (SQLException e) {
+            System.out.println("SQL Query Error!!!");
+        }
+        return word;
+    }
+
+    public static String changePassword(String login, String password) {
+        String word = null;
+        try {
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, password);
+            stmt.setString(2, login);
+            stmt.execute();
+        } catch (SQLException e) {
+            System.out.println("SQL Query Error!!!");
+        }
+        return word;
+    }
+}
